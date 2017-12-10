196e05b60458f3cf034891267b05be05cb92bdda
AlasAClass <lewis17scott@gmail.com>
2017-12-09 23:53:27 -0500

    Created a TODO for my acticity's functionality

--- app/src/main/java/com/example/antho/android_final/AutomobileActivity.java
+++ app/src/main/java/com/example/antho/android_final/AutomobileActivity.java
@@ -4,7 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 
-//Lewis Last Edit on Dec 7th 
+//Lewis Last Edit on Dec 9th
+//User is able to select LITRES, PRICE, KILOMETRES
+//entries displayed in a listview
+//database stores the time the information was recorded
+//app should provide AVERAGE GAS PRICE for last month
+//how much gas was purchased LAST MONTH
+//AVERAGE GAS PER MONTH(?)
 
 public class AutomobileActivity extends AppCompatActivity {
 
