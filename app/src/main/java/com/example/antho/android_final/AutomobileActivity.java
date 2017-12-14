07e6593041dcc0e7b31ef744112744ee66c64437
AlasAClass <lewis17scott@gmail.com>
2017-12-14 12:03:40 -0500

    Fleshed out the rest of the requirements for my activity

--- app/src/main/java/com/example/antho/android_final/AutomobileActivity.java
+++ app/src/main/java/com/example/antho/android_final/AutomobileActivity.java
@@ -3,7 +3,19 @@ package com.example.antho.android_final;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//GENERAL REQUIREMENTS
+//A Fragment
+//Listview to present items; clicking shows item details
+//Items in the listview must be stored, adding and deletion of items
+//An Asynctask(open a database/retrieve data/save data)
+//Progressbar
+//Button
+//EditText and an associated text input method
+//Toast, Snackbar, custom dialog notification
+//Help menu that shows author, activity number and interface instructions
+//alternate language 
 
+//ACTIVITY REQUIREMENTS
 //Lewis Last Edit on Dec 9th
 //User is able to select LITRES, PRICE, KILOMETRES
 //entries displayed in a listview
