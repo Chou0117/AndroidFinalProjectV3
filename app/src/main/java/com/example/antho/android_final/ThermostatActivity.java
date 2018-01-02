c7bec3cc0b0e9b5249656f93c3ff1fd0ca30ff71
chou0117 <johnny79622@gmail.com>
2018-01-02 17:23:54 -0500

    1234567890

--- app/src/main/java/com/example/antho/android_final/ThermostatActivity.java
+++ app/src/main/java/com/example/antho/android_final/ThermostatActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
-public class ThermostatActivity extends AppCompatActivity {
+public class  ThermostatActivity extends AppCompatActivity {
 
     private Toolbar topToolbar;
 
